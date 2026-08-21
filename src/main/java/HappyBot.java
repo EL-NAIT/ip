ba1fd6b2817bdcee59515966a4ade4f2e07bef32
EL-NAIT <yu.tianle723@gmail.com>
2026-08-21 12:51:56 +0800

    Level-1: Add Echo functionality
    HappyBot now echo commands entered by the user.
    Program exits when user types "bye"

--- src/main/java/HappyBot.java
+++ src/main/java/HappyBot.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Starts the HappyBot chatbot application.
  */
 public class HappyBot {
     // Codex was used to generate ASCII art for HAPPYBOT, rename file to HappyBot.java,
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String divider = "____________________________________________________________";
         String banner = "H   H   AAA   PPPP   PPPP   Y     Y BBBB    OOO   TTTTT\n"
                 + "H   H  A   A  P   P  P   P   Y   Y  B   B  O   O    T\n"
@@ -15,6 +18,16 @@ public class HappyBot {
         System.out.println("Hello! I'm HappyBot.");
         System.out.println("How can I cheer you up today?");
         System.out.println(divider);
+        while (true) {
+            String userInput = scanner.nextLine();
+            System.out.println(divider);
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.printf("%s\n%s\n", userInput, divider);
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
     }
